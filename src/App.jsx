36380db75ba3c62eda7ad2990fd28909e6e58d9e
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2023-06-18 09:30:22 +0700

    Provide the Redux Store to React

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,29 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import { Provider } from "react-redux";
+import { store } from "./store";
 // router
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<h1>this is the index route</h1>}></Route>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+
+      <Route path="/cart" index element={<Cart />} />
+      <Route path="/login" index element={<Login />} />
+    </Route>
   )
 );
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />;
+    </Provider>
+  );
 }
 
 export default App;
